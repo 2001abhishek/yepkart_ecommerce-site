da15461fdd55e9174c52c77d3f7f177e9af8917d
appy <sahuaparna1234@gmail.com>
2024-06-26 13:11:01 +0530

    update

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -6,30 +6,31 @@ function ProductCard() {
       name: "JEANS",
       icon: "https://5.imimg.com/data5/CB/XM/MY-62959177/denim-shorts-man.jpg",
     },
-    { name: "DRESSES", icon: "/path/to/dresses-icon.png" },
-    { name: "SHIRTS", icon: "/.0/to/shirts-icon.png" },
-    { name: "TOPS", icon: "/path/to/tops-icon.png" },
+    { name: "DRESSES", icon: "https://i.pinimg.com/564x/0f/f2/e5/0ff2e5df344a229d8f0febebe66cebe3.jpg" },
+    { name: "SHIRTS", icon: "https://getketchadmin.getketch.com/product/8905040506708/660/HLSH011340_4.JPG" },
+    { name: "TOPS", icon: "https://images.glowroad.com/faceview/hj/bf/hj/imgs/6b28574e-7549-4158-a707-01f0f1cb2acd_791579-xlgn400x400.jpg?productId=P-7894656" },
   ];
 
   return (
     <div className="w-full">
       {/* Category Bar */}
       <div className="bg-yellow-500 py-4">
-        <div className="container mx-auto flex justify-around items-center ">
-          {categories.map((category, index) => (
-            <div key={index} className="flex flex-col items-center ">
-              <img
-                src={category.icon}
-                alt={category.name}
-                className="w-8 h-8 mb-2 hidden md:block rounded-full"
-              />
-              <span className="text-white text-sm md:text-base">
-                {category.name} 
-              </span>
-            </div>
-          ))}
-        </div>
+  <div className="container mx-auto flex flex-wrap justify-center items-center space-x-72">
+    {categories.map((category, index) => (
+      <div key={index} className="cursor-pointer flex flex-col items-center m-4">
+        <img
+          src={category.icon}
+          alt={category.name}
+          className="w-20 h-20 mb-2 hidden md:block rounded-full"
+        />
+        <span className="text-white text-sm md:text-base">
+          {category.name}
+        </span>
       </div>
+    ))}
+  </div>
+</div>
+
 
       {/* Product Card Content */}
       <div className="bg-white shadow-md rounded-lg p-4 mt-4">
